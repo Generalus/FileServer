a369256dd755e98c2330a905b5b1a65593abc45b
Generalus <thesn@yandex.ru>
2017-03-04 23:40:47 +0300

    Update Connector.java

--- TestClient/src/main/java/ru/thesn/test_client/command/Connector.java
+++ TestClient/src/main/java/ru/thesn/test_client/command/Connector.java
@@ -9,11 +9,12 @@ import java.io.IOException;
 
 final class Connector {
 
+    private static final OkHttpClient client = new OkHttpClient();
+    
     private Connector() {
     }
 
     static Response connect(String url) throws IOException {
-        OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(url).build();
         Response response = client.newCall(request).execute();
         if (!response.isSuccessful()) {
